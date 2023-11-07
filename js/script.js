7cc7dcda5a7a2323c1260f6480a24e029690e90b
Veronika Ulybina <veronica.ulybina@yandex.ru>
2023-11-07 15:22:43 +0500

    init project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const nameProduct = 'navigation system';
+const amount = 7;
+const category = 'home appliances';
+const price = 100;
+const totalAmount = amount * price;
+
+console.log(nameProduct);
+console.log(`Общая сумма ${nameProduct}: ${totalAmount}$`);
